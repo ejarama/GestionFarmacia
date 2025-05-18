6d657e153f5807317fae29b9345c34f866a70aea
Sammy Valeria Arteaga Velasquez <sammy.arteaga081@pascualbravo.edu.co>
2025-05-18 15:26:39 -0500

    se hizo el FrmReportes

--- GestionFarmacia/Forms/FrmReportes.Designer.cs
+++ GestionFarmacia/Forms/FrmReportes.Designer.cs
@@ -29,12 +29,139 @@ namespace GestionFarmacia.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblReportes = new System.Windows.Forms.Label();
+            this.lblFechaInicio = new System.Windows.Forms.Label();
+            this.lblFechaFin = new System.Windows.Forms.Label();
+            this.lblFiltro = new System.Windows.Forms.Label();
+            this.dtpFechaInicio = new System.Windows.Forms.DateTimePicker();
+            this.dtpFechaFin = new System.Windows.Forms.DateTimePicker();
+            this.cmbFiltro = new System.Windows.Forms.ComboBox();
+            this.btnGenerarReporte = new System.Windows.Forms.Button();
+            this.lblReporte = new System.Windows.Forms.Label();
+            this.dgvReporte = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblReportes
+            // 
+            this.lblReportes.AutoSize = true;
+            this.lblReportes.Location = new System.Drawing.Point(272, 22);
+            this.lblReportes.Name = "lblReportes";
+            this.lblReportes.Size = new System.Drawing.Size(63, 16);
+            this.lblReportes.TabIndex = 0;
+            this.lblReportes.Text = "Reportes";
+            // 
+            // lblFechaInicio
+            // 
+            this.lblFechaInicio.AutoSize = true;
+            this.lblFechaInicio.Location = new System.Drawing.Point(81, 87);
+            this.lblFechaInicio.Name = "lblFechaInicio";
+            this.lblFechaInicio.Size = new System.Drawing.Size(82, 16);
+            this.lblFechaInicio.TabIndex = 1;
+            this.lblFechaInicio.Text = "Fecha Inicio:";
+            // 
+            // lblFechaFin
+            // 
+            this.lblFechaFin.AutoSize = true;
+            this.lblFechaFin.Location = new System.Drawing.Point(84, 137);
+            this.lblFechaFin.Name = "lblFechaFin";
+            this.lblFechaFin.Size = new System.Drawing.Size(72, 16);
+            this.lblFechaFin.TabIndex = 3;
+            this.lblFechaFin.Text = "Fecha Fin: ";
+            // 
+            // lblFiltro
+            // 
+            this.lblFiltro.AutoSize = true;
+            this.lblFiltro.Location = new System.Drawing.Point(94, 218);
+            this.lblFiltro.Name = "lblFiltro";
+            this.lblFiltro.Size = new System.Drawing.Size(62, 16);
+            this.lblFiltro.TabIndex = 4;
+            this.lblFiltro.Text = "Filtro por:";
+            // 
+            // dtpFechaInicio
+            // 
+            this.dtpFechaInicio.Location = new System.Drawing.Point(202, 82);
+            this.dtpFechaInicio.Name = "dtpFechaInicio";
+            this.dtpFechaInicio.Size = new System.Drawing.Size(255, 22);
+            this.dtpFechaInicio.TabIndex = 5;
+            // 
+            // dtpFechaFin
+            // 
+            this.dtpFechaFin.Location = new System.Drawing.Point(202, 131);
+            this.dtpFechaFin.Name = "dtpFechaFin";
+            this.dtpFechaFin.Size = new System.Drawing.Size(255, 22);
+            this.dtpFechaFin.TabIndex = 6;
+            // 
+            // cmbFiltro
+            // 
+            this.cmbFiltro.FormattingEnabled = true;
+            this.cmbFiltro.Location = new System.Drawing.Point(178, 214);
+            this.cmbFiltro.Name = "cmbFiltro";
+            this.cmbFiltro.Size = new System.Drawing.Size(121, 24);
+            this.cmbFiltro.TabIndex = 7;
+            // 
+            // btnGenerarReporte
+            // 
+            this.btnGenerarReporte.Location = new System.Drawing.Point(322, 215);
+            this.btnGenerarReporte.Name = "btnGenerarReporte";
+            this.btnGenerarReporte.Size = new System.Drawing.Size(145, 23);
+            this.btnGenerarReporte.TabIndex = 8;
+            this.btnGenerarReporte.Text = "generar el reporte";
+            this.btnGenerarReporte.UseVisualStyleBackColor = true;
+            // 
+            // lblReporte
+            // 
+            this.lblReporte.AutoSize = true;
+            this.lblReporte.Location = new System.Drawing.Point(97, 289);
+            this.lblReporte.Name = "lblReporte";
+            this.lblReporte.Size = new System.Drawing.Size(132, 16);
+            this.lblReporte.TabIndex = 9;
+            this.lblReporte.Text = " Reporte Generado:  ";
+            // 
+            // dgvReporte
+            // 
+            this.dgvReporte.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvReporte.Location = new System.Drawing.Point(249, 272);
+            this.dgvReporte.Name = "dgvReporte";
+            this.dgvReporte.RowHeadersWidth = 51;
+            this.dgvReporte.RowTemplate.Height = 24;
+            this.dgvReporte.Size = new System.Drawing.Size(240, 150);
+            this.dgvReporte.TabIndex = 10;
+            // 
+            // FrmReportes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvReporte);
+            this.Controls.Add(this.lblReporte);
+            this.Controls.Add(this.btnGenerarReporte);
+            this.Controls.Add(this.cmbFiltro);
+            this.Controls.Add(this.dtpFechaFin);
+            this.Controls.Add(this.dtpFechaInicio);
+            this.Controls.Add(this.lblFiltro);
+            this.Controls.Add(this.lblFechaFin);
+            this.Controls.Add(this.lblFechaInicio);
+            this.Controls.Add(this.lblReportes);
+            this.Name = "FrmReportes";
             this.Text = "FrmReportes";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvReporte)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblReportes;
+        private System.Windows.Forms.Label lblFechaInicio;
+        private System.Windows.Forms.Label lblFechaFin;
+        private System.Windows.Forms.Label lblFiltro;
+        private System.Windows.Forms.DateTimePicker dtpFechaInicio;
+        private System.Windows.Forms.DateTimePicker dtpFechaFin;
+        private System.Windows.Forms.ComboBox cmbFiltro;
+        private System.Windows.Forms.Button btnGenerarReporte;
+        private System.Windows.Forms.Label lblReporte;
+        private System.Windows.Forms.DataGridView dgvReporte;
     }
 }
