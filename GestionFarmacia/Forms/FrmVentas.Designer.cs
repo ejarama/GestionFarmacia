73b06e12e5f4d482048091d8bd9fe3009dbacfb7
Sammy Valeria Arteaga Velasquez <sammy.arteaga081@pascualbravo.edu.co>
2025-05-18 15:34:14 -0500

    se hizo el FrmVentas

--- GestionFarmacia/Forms/FrmVentas.Designer.cs
+++ GestionFarmacia/Forms/FrmVentas.Designer.cs
@@ -29,12 +29,159 @@ namespace GestionFarmacia.Forms
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.lblRegistar = new System.Windows.Forms.Label();
+            this.lblProducto = new System.Windows.Forms.Label();
+            this.lblCantidad = new System.Windows.Forms.Label();
+            this.lblPrecioTotal = new System.Windows.Forms.Label();
+            this.cmbProducto = new System.Windows.Forms.ComboBox();
+            this.numCantidad = new System.Windows.Forms.NumericUpDown();
+            this.txtPrecioTotal = new System.Windows.Forms.TextBox();
+            this.btnAgregarProducto = new System.Windows.Forms.Button();
+            this.btnFinalizarVenta = new System.Windows.Forms.Button();
+            this.btnCancelarVenta = new System.Windows.Forms.Button();
+            this.dgvProductosVenta = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductosVenta)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblRegistar
+            // 
+            this.lblRegistar.AutoSize = true;
+            this.lblRegistar.Location = new System.Drawing.Point(314, 22);
+            this.lblRegistar.Name = "lblRegistar";
+            this.lblRegistar.Size = new System.Drawing.Size(128, 16);
+            this.lblRegistar.TabIndex = 0;
+            this.lblRegistar.Text = " Registro de Ventas ";
+            // 
+            // lblProducto
+            // 
+            this.lblProducto.AutoSize = true;
+            this.lblProducto.Location = new System.Drawing.Point(104, 109);
+            this.lblProducto.Name = "lblProducto";
+            this.lblProducto.Size = new System.Drawing.Size(61, 16);
+            this.lblProducto.TabIndex = 1;
+            this.lblProducto.Text = "Producto";
+            // 
+            // lblCantidad
+            // 
+            this.lblCantidad.AutoSize = true;
+            this.lblCantidad.Location = new System.Drawing.Point(104, 152);
+            this.lblCantidad.Name = "lblCantidad";
+            this.lblCantidad.Size = new System.Drawing.Size(61, 16);
+            this.lblCantidad.TabIndex = 2;
+            this.lblCantidad.Text = "Cantidad";
+            // 
+            // lblPrecioTotal
+            // 
+            this.lblPrecioTotal.AutoSize = true;
+            this.lblPrecioTotal.Location = new System.Drawing.Point(90, 202);
+            this.lblPrecioTotal.Name = "lblPrecioTotal";
+            this.lblPrecioTotal.Size = new System.Drawing.Size(93, 16);
+            this.lblPrecioTotal.TabIndex = 3;
+            this.lblPrecioTotal.Text = "Precio Total: $";
+            // 
+            // cmbProducto
+            // 
+            this.cmbProducto.FormattingEnabled = true;
+            this.cmbProducto.Location = new System.Drawing.Point(211, 101);
+            this.cmbProducto.Name = "cmbProducto";
+            this.cmbProducto.Size = new System.Drawing.Size(121, 24);
+            this.cmbProducto.TabIndex = 4;
+            // 
+            // numCantidad
+            // 
+            this.numCantidad.Location = new System.Drawing.Point(211, 152);
+            this.numCantidad.Name = "numCantidad";
+            this.numCantidad.Size = new System.Drawing.Size(120, 22);
+            this.numCantidad.TabIndex = 5;
+            this.numCantidad.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // txtPrecioTotal
+            // 
+            this.txtPrecioTotal.Location = new System.Drawing.Point(211, 199);
+            this.txtPrecioTotal.Name = "txtPrecioTotal";
+            this.txtPrecioTotal.Size = new System.Drawing.Size(100, 22);
+            this.txtPrecioTotal.TabIndex = 6;
+            // 
+            // btnAgregarProducto
+            // 
+            this.btnAgregarProducto.Location = new System.Drawing.Point(136, 263);
+            this.btnAgregarProducto.Name = "btnAgregarProducto";
+            this.btnAgregarProducto.Size = new System.Drawing.Size(139, 23);
+            this.btnAgregarProducto.TabIndex = 7;
+            this.btnAgregarProducto.Text = "Agregar Producto";
+            this.btnAgregarProducto.UseVisualStyleBackColor = true;
+            // 
+            // btnFinalizarVenta
+            // 
+            this.btnFinalizarVenta.Location = new System.Drawing.Point(317, 263);
+            this.btnFinalizarVenta.Name = "btnFinalizarVenta";
+            this.btnFinalizarVenta.Size = new System.Drawing.Size(125, 23);
+            this.btnFinalizarVenta.TabIndex = 8;
+            this.btnFinalizarVenta.Text = "Finalizar Venta";
+            this.btnFinalizarVenta.UseVisualStyleBackColor = true;
+            // 
+            // btnCancelarVenta
+            // 
+            this.btnCancelarVenta.Location = new System.Drawing.Point(475, 263);
+            this.btnCancelarVenta.Name = "btnCancelarVenta";
+            this.btnCancelarVenta.Size = new System.Drawing.Size(115, 23);
+            this.btnCancelarVenta.TabIndex = 9;
+            this.btnCancelarVenta.Text = "Cancelar Venta";
+            this.btnCancelarVenta.UseVisualStyleBackColor = true;
+            // 
+            // dgvProductosVenta
+            // 
+            this.dgvProductosVenta.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvProductosVenta.Location = new System.Drawing.Point(93, 310);
+            this.dgvProductosVenta.Name = "dgvProductosVenta";
+            this.dgvProductosVenta.ReadOnly = true;
+            this.dgvProductosVenta.RowHeadersWidth = 51;
+            this.dgvProductosVenta.RowTemplate.Height = 24;
+            this.dgvProductosVenta.Size = new System.Drawing.Size(520, 117);
+            this.dgvProductosVenta.TabIndex = 10;
+            // 
+            // FrmVentas
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvProductosVenta);
+            this.Controls.Add(this.btnCancelarVenta);
+            this.Controls.Add(this.btnFinalizarVenta);
+            this.Controls.Add(this.btnAgregarProducto);
+            this.Controls.Add(this.txtPrecioTotal);
+            this.Controls.Add(this.numCantidad);
+            this.Controls.Add(this.cmbProducto);
+            this.Controls.Add(this.lblPrecioTotal);
+            this.Controls.Add(this.lblCantidad);
+            this.Controls.Add(this.lblProducto);
+            this.Controls.Add(this.lblRegistar);
+            this.Name = "FrmVentas";
             this.Text = "FrmVentas";
+            ((System.ComponentModel.ISupportInitialize)(this.numCantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvProductosVenta)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblRegistar;
+        private System.Windows.Forms.Label lblProducto;
+        private System.Windows.Forms.Label lblCantidad;
+        private System.Windows.Forms.Label lblPrecioTotal;
+        private System.Windows.Forms.ComboBox cmbProducto;
+        private System.Windows.Forms.NumericUpDown numCantidad;
+        private System.Windows.Forms.TextBox txtPrecioTotal;
+        private System.Windows.Forms.Button btnAgregarProducto;
+        private System.Windows.Forms.Button btnFinalizarVenta;
+        private System.Windows.Forms.Button btnCancelarVenta;
+        private System.Windows.Forms.DataGridView dgvProductosVenta;
     }
 }
