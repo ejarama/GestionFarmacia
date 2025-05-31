063295a30f9a1d404f639ee6fd3679c7660e413f
Catalina <catalina.correa346@pascualbravo.edu.co>
2025-05-31 11:50:11 -0500

    Implementación del patrón Unit of Work para que controle las operaciones de una venta

---
+++ GestionFarmacia/Data/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GestionFarmacia.Data;
+using GestionFarmacia.Data.Interfaces;
+using GestionFarmacia.Data.Repositories;
+using GestionFarmacia.Entities;
+
+namespace GestionFarmacia
+{
+    public interface IUnitOfWork
+    {
+        bool RegistrarVenta(Venta venta);
+    }
+}
